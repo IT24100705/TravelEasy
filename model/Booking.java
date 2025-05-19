970f598f9b8774a4e963c6c182421b9f99bcb4bb
it24103115 <it24103115@my.sliit.lk>
2025-05-19 16:58:47 +0530

    Added Booking

---
+++ model/Booking.java
@@ -0,0 +1,45 @@
+package model;
+
+public class Booking {
+    private String bookingId;
+    private String userName;
+    private String contact;
+    private String packageId;
+    private String packageTitle;
+    private String date;
+    private double totalPrice;
+
+    public Booking(String bookingId, String userName, String contact, String packageId,
+                   String packageTitle, String date, double totalPrice) {
+        this.bookingId = bookingId;
+        this.userName = userName;
+        this.contact = contact;
+        this.packageId = packageId;
+        this.packageTitle = packageTitle;
+        this.date = date;
+        this.totalPrice = totalPrice;
+    }
+
+    // Getters
+    public String getBookingId() {
+        return bookingId;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public String getContact() {
+        return contact;
+    }
+    public String getPackageId() {
+        return packageId;
+    }
+    public String getPackageTitle() {
+        return packageTitle;
+    }
+    public String getDate() {
+        return date;
+    }
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+}
