cc0490d52506d20f0d7412ac3035172045d8a211
it24100749 <it24100749@my.sliit.lk>
2025-05-21 09:55:40 +0530

    Update DeleteUserServlet.java

--- src/main/java/servlets/DeleteUserServlet.java
+++ src/main/java/servlets/DeleteUserServlet.java
@@ -2,13 +2,14 @@ package servlets;
 
 import utils.FileUtils;
 
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
 import java.io.*;
 
+@WebServlet(name = "DeleteUserServlet",value = "/delete")
 public class DeleteUserServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        String username = (String) req.getSession().getAttribute("username");
+        String username = (String) req.getSession().getAttribute("email");
 
         if (username == null) {
             res.sendRedirect("login.jsp?message=Please+log+in+first");
